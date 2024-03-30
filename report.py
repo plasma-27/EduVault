ce363f94d3e5432456ff9e5f066a6a03fe7a7c00
Alok <2022.alok.yadav@ves.ac.in>
2024-03-30 10:06:56 +0530

    added report submission

--- report.py
+++ report.py
@@ -19,15 +19,41 @@ class Report:
             print(f"Error fetching reports: {e}")
         finally:
             # Close the database connection
-            if mydb:
+            if mydb:      
                 mydb.close()
 
         return reports_list
+    
+    def submit_report(iuid, suid, message):
+        try:
+            dbobj = db()
+            mydb, cursor = dbobj.dbconnect("credentials")
+            
+            # Insert report data into the database with action_taken set to 0
+            query = "INSERT INTO reports (reported_by, reported_to, message, action_taken) VALUES (%s, %s, %s, %s)"
+            cursor.execute(query, (iuid, suid, message, 0))
+            mydb.commit()
+            report_id = cursor.lastrowid
 
-# Example usage:
-reports = Report.view_report()
+            print("Report submitted successfully!")
+            return report_id
 
-# Display the reports
-for report_data in reports:
-    report_id, reported_by, reported_to, message, action_taken = report_data
-    print(f"Report ID: {report_id}, Reported By: {reported_by}, Reported To: {reported_to}, Message: {message}, Action Taken: {action_taken}")
+        except Exception as e:
+            print(f"Error submitting report: {e}")
+            return None
+        finally:
+            # Close the database connection
+            if mydb:
+                mydb.close()
+
+
+
+
+
+# # Example usage:
+# reports = Report.view_report()
+
+# # Display the reports
+# for report_data in reports:
+#     report_id, reported_by, reported_to, message, action_taken = report_data
+#     print(f"Report ID: {report_id}, Reported By: {reported_by}, Reported To: {reported_to}, Message: {message}, Action Taken: {action_taken}")
